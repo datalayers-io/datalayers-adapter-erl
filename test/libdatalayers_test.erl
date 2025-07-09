810796ec6e4155c9f1da3d299fcdfb21ed4784c2
JimMoen <LnJimMoen@outlook.com>
2025-07-09 14:43:57 +0800

    test: prepare and execute prepared

--- test/libdatalayers_test.erl
+++ test/libdatalayers_test.erl
@@ -40,3 +40,25 @@ stop_test_() ->
             )
         end
     }.
+
+prepare_test_() ->
+    {
+        "Prepare and execute",
+        fun() ->
+            Host = <<"172.19.0.30">>,
+            Port = 8360,
+            Username = <<"admin">>,
+            Password = <<"public">>,
+            {ok, Client} = libdatalayers:connect(#{
+                host => Host,
+                port => Port,
+                username => Username,
+                password => Password
+            }),
+            {ok, PreparedStatement} = libdatalayers:prepare(Client, <<"INSERT INTO rust.demo (ts, sid, value, flag) VALUES (?, ?, ?, ?);">>),
+            ok = libdatalayers:execute_prepared(Client, PreparedStatement, [
+                [<<"2023-10-01 12:00:00">>, 1, 42.0, 1],
+                [<<"2023-10-01 12:05:00">>, 2, 43.0, 0]
+            ]),
+        end
+    }.

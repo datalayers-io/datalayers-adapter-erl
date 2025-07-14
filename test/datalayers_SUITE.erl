fd31a74a572ac558e54d73573ff12f81e47ce0de
JimMoen <LnJimMoen@outlook.com>
2025-07-14 19:33:26 +0800

    test: datalayers connect/execute/prepare as common test

--- test/datalayers_SUITE.erl
+++ test/datalayers_SUITE.erl
@@ -1,30 +1,49 @@
 -module(datalayers_SUITE).
+
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
 
--define(host, <<"172.18.0.2">>).
+-define(host, get_host_addr()).
 
-connect_test_() ->
-    {
-        "Connect and execute",
-        fun() ->
-            {ok, Client} = datalayers:connect(#{host => ?host}),
-            {ok, _} = datalayers:execute(Client, <<"CREATE DATABASE eunit_test">>),
-            {ok, [[<<"2.3.3">>]]} = datalayers:execute(Client, <<"SELECT version()">>),
-            {ok, _} = datalayers:execute(Client, <<"DROP DATABASE eunit_test">>)
-        end
-    }.
+-compile([export_all, nowarn_export_all]).
 
-stop_test_() ->
-    {
-        "Stop client",
-        fun() ->
-            {ok, Client} = datalayers:connect(#{host => ?host}),
-            ok = datalayers:stop(Client),
-            {error, <<"client_stopped">>} = datalayers:execute(
-                Client, <<"CREATE DATABASE eunit_test">>
-            )
-        end
-    }.
+suite() -> [].
+
+all() -> [connect_test, stop_test, prepare_test].
+
+groups() -> [].
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+init_per_group(_GroupName, Config) ->
+    Config.
+
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+connect_test(_Config) ->
+    {ok, Client} = datalayers:connect(#{host => ?host}),
+    {ok, _} = datalayers:execute(Client, <<"CREATE DATABASE eunit_test">>),
+    {ok, [[Version]]} = datalayers:execute(Client, <<"SELECT version()">>),
+    ?assert(is_binary(Version)),
+    {ok, _} = datalayers:execute(Client, <<"DROP DATABASE eunit_test">>).
+
+stop_test(_Config) ->
+    {ok, Client} = datalayers:connect(#{host => ?host}),
+    ok = datalayers:stop(Client),
+    {error, <<"client_stopped">>} = datalayers:execute(
+        Client, <<"CREATE DATABASE eunit_test">>
+    ).
 
 %% erlfmt-ignore
 -define(create_database, <<"
@@ -39,25 +58,26 @@ stop_test_() ->
     ENGINE=TimeSeries;
 ">>).
 
-prepare_test_() ->
-    {
-        "Prepare and execute",
-        fun() ->
-            {ok, Client} = datalayers:connect(#{host => ?host}),
-            {ok, _} = datalayers:execute(Client, <<"CREATE DATABASE eunit_test">>),
-            {ok, _} = datalayers:execute(Client, ?create_database),
-            {ok, PreparedStatement} = datalayers:prepare(
-                Client,
-                <<"INSERT INTO eunit_test.demo (ts, sid, value, flag) VALUES (?, ?, ?, ?);">>
-            ),
-            {ok, _} = datalayers:execute_prepare(Client, PreparedStatement, [
-                [erlang:system_time(millisecond), 1, 42.0, 1],
-                [erlang:system_time(millisecond), 2, 43.0, 0]
-            ]),
-            {ok, _} = datalayers:close_prepared(Client, PreparedStatement),
-            {ok, [[<<"2.3.3">>]]} = datalayers:execute(Client, <<"SELECT version()">>),
-            {ok, _} = datalayers:execute(Client, <<"DROP TABLE eunit_test.demo">>),
-            {ok, _} = datalayers:execute(Client, <<"DROP DATABASE eunit_test">>),
-            ok = datalayers:stop(Client)
-        end
-    }.
+prepare_test(_Config) ->
+    {ok, Client} = datalayers:connect(#{host => ?host}),
+    {ok, _} = datalayers:execute(Client, <<"CREATE DATABASE eunit_test">>),
+    {ok, _} = datalayers:execute(Client, ?create_database),
+    {ok, PreparedStatement} = datalayers:prepare(
+        Client,
+        <<"INSERT INTO eunit_test.demo (ts, sid, value, flag) VALUES (?, ?, ?, ?);">>
+    ),
+    {ok, _} = datalayers:execute_prepare(Client, PreparedStatement, [
+        [erlang:system_time(millisecond), 1, 42.0, 1],
+        [erlang:system_time(millisecond), 2, 43.0, 0]
+    ]),
+    {ok, _} = datalayers:close_prepared(Client, PreparedStatement),
+    {ok, [[<<"2.3.3">>]]} = datalayers:execute(Client, <<"SELECT version()">>),
+    {ok, _} = datalayers:execute(Client, <<"DROP TABLE eunit_test.demo">>),
+    {ok, _} = datalayers:execute(Client, <<"DROP DATABASE eunit_test">>),
+    ok = datalayers:stop(Client).
+
+get_host_addr() ->
+    case os:getenv("DATALAYERS_TCP_ADDR") of
+        false -> <<"localhost">>;
+        Host -> iolist_to_binary(Host)
+    end.

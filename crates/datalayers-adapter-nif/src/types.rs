20e85ed2847a80e58b44db627ef7d409ccff5c2f
JimMoen <LnJimMoen@outlook.com>
2025-07-30 15:48:00 +0800

    refactor: use macro to simplify types handling functions

--- crates/datalayers-adapter-nif/src/types.rs
+++ crates/datalayers-adapter-nif/src/types.rs
@@ -7,190 +7,84 @@ use arrow_array::builder::{
 use arrow_schema::{DataType, TimeUnit};
 use rustler::{Error, Term};
 
-pub fn get_array_builder(data_type: &DataType) -> Result<Box<dyn ArrayBuilder>, Error> {
-    // https://docs.datalayers.cn/datalayers/latest/sql-reference/data-type.html
-    match data_type {
-        DataType::Int8 => Ok(Box::new(Int8Builder::new())),
-        DataType::Int16 => Ok(Box::new(Int16Builder::new())),
-        DataType::Int32 => Ok(Box::new(Int32Builder::new())),
-        DataType::Int64 => Ok(Box::new(Int64Builder::new())),
-
-        DataType::UInt8 => Ok(Box::new(UInt8Builder::new())),
-        DataType::UInt16 => Ok(Box::new(UInt16Builder::new())),
-        DataType::UInt32 => Ok(Box::new(UInt32Builder::new())),
-        DataType::UInt64 => Ok(Box::new(UInt64Builder::new())),
-
-        DataType::Float32 => Ok(Box::new(Float32Builder::new())),
-        DataType::Float64 => Ok(Box::new(Float64Builder::new())),
-
-        DataType::Timestamp(TimeUnit::Second, Some(zone)) => Ok(Box::new(
-            TimestampSecondBuilder::new().with_timezone(zone.clone()),
-        )),
-        DataType::Timestamp(TimeUnit::Millisecond, Some(zone)) => Ok(Box::new(
-            TimestampMillisecondBuilder::new().with_timezone(zone.clone()),
-        )),
-        DataType::Timestamp(TimeUnit::Microsecond, Some(zone)) => Ok(Box::new(
-            TimestampMicrosecondBuilder::new().with_timezone(zone.clone()),
-        )),
-        DataType::Timestamp(TimeUnit::Nanosecond, Some(zone)) => Ok(Box::new(
-            TimestampNanosecondBuilder::new().with_timezone(zone.clone()),
-        )),
-
-        DataType::Boolean => Ok(Box::new(BooleanBuilder::new())),
-
-        DataType::Utf8 => Ok(Box::new(StringBuilder::new())),
-
-        unimplemented => Err(Error::Term(Box::new(format!(
-            "unsupported type: {unimplemented}"
-        )))),
-    }
-}
-
-pub fn append_value_to_builder(
-    builder: &mut Box<dyn ArrayBuilder>,
-    term: Term,
-    data_type: &DataType,
-) -> Result<(), Error> {
-    match data_type {
-        DataType::Int8 => {
-            let val = term.decode::<i8>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<Int8Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Int16 => {
-            let val = term.decode::<i16>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<Int16Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Int32 => {
-            let val = term.decode::<i32>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<Int32Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Int64 => {
-            let val = term.decode::<i64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<Int64Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
+macro_rules! define_type_handling_functions {
+    (
+        standard_types: [ $( ($dt:ident, $builder:ident, $rust_ty:ty) ),* $(,)? ],
+        timestamp_types: [ $( ($unit:ident, $ts_builder:ident) ),* $(,)? ]
+    ) => {
+        pub fn get_array_builder(data_type: &DataType) -> Result<Box<dyn ArrayBuilder>, Error> {
+            match data_type {
+                $(
+                    DataType::$dt => Ok(Box::new($builder::new())),
+                )*
+                $(
+                    DataType::Timestamp(TimeUnit::$unit, Some(zone)) => Ok(Box::new(
+                        $ts_builder::new().with_timezone(zone.clone()),
+                    )),
+                )*
+                unimplemented => Err(Error::Term(Box::new(format!(
+                    "unsupported type: {unimplemented}"
+                )))),
+            }
         }
 
-        DataType::UInt8 => {
-            let val = term.decode::<u8>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<UInt8Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::UInt16 => {
-            let val = term.decode::<u16>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<UInt16Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::UInt32 => {
-            let val = term.decode::<u32>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<UInt32Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::UInt64 => {
-            let val = term.decode::<u64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<UInt64Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-
-        DataType::Float32 => {
-            let val = term.decode::<f32>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<Float32Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Float64 => {
-            let val = term.decode::<f64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<Float64Builder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-
-        DataType::Timestamp(TimeUnit::Second, _) => {
-            let val = term.decode::<i64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<TimestampSecondBuilder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Timestamp(TimeUnit::Millisecond, _) => {
-            let val = term.decode::<i64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<TimestampMillisecondBuilder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Timestamp(TimeUnit::Microsecond, _) => {
-            let val = term.decode::<i64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<TimestampMicrosecondBuilder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-        DataType::Timestamp(TimeUnit::Nanosecond, _) => {
-            let val = term.decode::<i64>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<TimestampNanosecondBuilder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-
-        DataType::Boolean => {
-            let val = term.decode::<bool>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<BooleanBuilder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-
-        DataType::Utf8 => {
-            let val = term.decode::<String>()?;
-            builder
-                .as_any_mut()
-                .downcast_mut::<StringBuilder>()
-                .ok_or(Error::BadArg)?
-                .append_value(val);
-        }
-
-        unimplemented => {
-            return Err(Error::Term(Box::new(format!(
-                "unsupported type: {unimplemented}"
-            ))));
+        pub fn append_value_to_builder(
+            builder: &mut Box<dyn ArrayBuilder>,
+            term: Term,
+            data_type: &DataType,
+        ) -> Result<(), Error> {
+            match data_type {
+                $(
+                    DataType::$dt => {
+                        let val = term.decode::<$rust_ty>()?;
+                        builder
+                            .as_any_mut()
+                            .downcast_mut::<$builder>()
+                            .ok_or(Error::BadArg)?
+                            .append_value(val);
+                    }
+                )*
+                $(
+                    DataType::Timestamp(TimeUnit::$unit, _) => {
+                        let val = term.decode::<i64>()?;
+                        builder
+                            .as_any_mut()
+                            .downcast_mut::<$ts_builder>()
+                            .ok_or(Error::BadArg)?
+                            .append_value(val);
+                    }
+                )*
+                unimplemented => {
+                    return Err(Error::Term(Box::new(format!(
+                        "unsupported type: {unimplemented}"
+                    ))));
+                }
+            };
+            Ok(())
         }
     };
-    Ok(())
+}
+
+// https://docs.datalayers.cn/datalayers/latest/sql-reference/data-type.html
+define_type_handling_functions! {
+    standard_types: [
+        (Int8, Int8Builder, i8),
+        (Int16, Int16Builder, i16),
+        (Int32, Int32Builder, i32),
+        (Int64, Int64Builder, i64),
+        (UInt8, UInt8Builder, u8),
+        (UInt16, UInt16Builder, u16),
+        (UInt32, UInt32Builder, u32),
+        (UInt64, UInt64Builder, u64),
+        (Float32, Float32Builder, f32),
+        (Float64, Float64Builder, f64),
+        (Boolean, BooleanBuilder, bool),
+        (Utf8, StringBuilder, String),
+    ],
+    timestamp_types: [
+        (Second, TimestampSecondBuilder),
+        (Millisecond, TimestampMillisecondBuilder),
+        (Microsecond, TimestampMicrosecondBuilder),
+        (Nanosecond, TimestampNanosecondBuilder),
+    ]
 }
